ff1b60ed982a46e57439334034ae426e79bd21ff
mg0678 <mguilmette@gmail.com>
2015-12-22 17:00:15 -0500

    - Got player URLs formatted correctly - Working on getting player data

--- NBA_Stats/src/nba_stats/NBA_Stats.java
+++ NBA_Stats/src/nba_stats/NBA_Stats.java
@@ -11,11 +11,17 @@ import org.jsoup.nodes.Element;
  * @author Michael.Guilmette
  */
 public class NBA_Stats {
-    
+    public static int ordinalIndexOf(String str, char c, int n) {
+    int pos = str.indexOf(c, 0);
+    while (n-- > 0 && pos != -1)
+        pos = str.indexOf(c, pos+1);
+    return pos;
+    }
     public static void main(String[] args) throws IOException {
         ArrayList<String> urls = new ArrayList<String>();
         ArrayList<String> teamUrls = new ArrayList<String>();
         ArrayList<String> players = new ArrayList<String>();
+        ArrayList<String> stats = new ArrayList<String>();
         
         org.jsoup.nodes.Document doc = Jsoup.connect("http://espn.go.com/nba/teams").get();
         org.jsoup.select.Elements classes = doc.select("a[href].bi");
@@ -33,17 +39,29 @@ public class NBA_Stats {
         for (int i = 0; i < teamUrls.size(); i++) {
             String x = teamUrls.get(i);
             x = x.substring(0, 27) + "/roster" + x.substring(27, x.length());
-            System.out.println(x);
+            //System.out.println(x);
             
             org.jsoup.nodes.Document plyrs = Jsoup.connect(x).get();
             org.jsoup.select.Elements player_urls = plyrs.select(".sortcell");
             
             for (Element e : player_urls){
-                players.add(e.attr("href"));
-                System.out.println(e);
-            }
+                String g = e.toString();
+                int beginIndex = ordinalIndexOf(g, '"' , 2);
+                int endIndex = ordinalIndexOf(g, '"' , 3);                
+                players.add(g.substring(beginIndex+1,endIndex));
+            }            
         }
+        /*for (int i = 0; i < players.size(); i++){
+            System.out.println(players.get(i));
+        }*/
         
-        System.out.println(players.size());
+        org.jsoup.nodes.Document stat = Jsoup.connect(players.get(0)).get();
+        org.jsoup.select.Elements player_stats = stat.select(".mod-content td");
+        for (Element e : player_stats){
+            String g = e.toString();
+            stats.add(g);
+            System.out.println(g);
+        }
+        System.out.println(players.get(0));
     }
 }
